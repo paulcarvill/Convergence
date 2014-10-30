5d5d68b15026e66695ec03c77f863dc4a1fbe830
Paul Carvill <paul@paulcarvill.com>
2014-10-30 13:38:01 +0000

    gemfile

--- Gemfile
+++ Gemfile
@@ -36,7 +36,7 @@ gem 'paperclip', github: 'thoughtbot/paperclip'
 gem 'paperclip-meta'
 
 group :production do
-  gem 'pg', '0.15.1'
+  gem 'mysql2'
   gem 'rails_12factor', '0.0.2'
   gem 'thin'
 end
--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -4,19 +4,5 @@ class PagesController < ApplicationController
   def index
     @events = Event.where("featured == ?", true)
     @moreEvents = Event.where("featured != ?", true).limit(5)
-
-    puts "* * * * * * * *"
-    puts "* * * * * * * *"
-    puts "* * * * * * * *"
-    puts "* * * * * * * *"
-    puts "* * * * * * * *"
-    puts "* * * * * * * *"
-    puts "* * * * * * * *"
-    puts "* * * * * * * *"
-    puts "* * * * * * * *"
-    puts "* * * * * * * *"
-    puts "* * * * * * * *"
-    puts "* * * * * * * *"
-    puts @moreEvents
   end
 end
