55553640a54383061134d28504b3a985d1858a36
Paul Carvill <paul@paulcarvill.com>
2014-11-01 18:06:12 +0000

    adding other pages - locations, about

--- config/routes.rb
+++ config/routes.rb
@@ -21,6 +21,8 @@ Rails.application.routes.draw do
 
   # You can have the root of your site routed with "root"
   root 'pages#index'
+  get 'about' => 'pages#about'
+  get 'locations' => 'pages#map'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
