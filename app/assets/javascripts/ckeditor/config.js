77134d92c8c8eb3e63a1ad4995d607558b13ae5a
Paul Carvill <paul@paulcarvill.com>
2014-11-02 22:45:12 +0000

    fixing ckeditor image uploads

--- app/assets/javascripts/ckeditor/config.js
+++ app/assets/javascripts/ckeditor/config.js
@@ -3,9 +3,16 @@ CKEDITOR.editorConfig = function( config ) {
 	console.log(config);
     config.toolbar_convergence = [
 	  ['Image'],
-	  ['Bold','Italic'],
+	  ['Bold','Italic', 'Underline'],
 	  ['NumberedList','BulletedList'],
-	  [ 'Link','Unlink','Anchor' ],
+	  [ 'Link','Unlink' ],
 	  ['Source']
-	]
+	],
+	config.filebrowserBrowseUrl = "/ckeditor/attachment_files";
+  config.filebrowserFlashBrowseUrl = "/ckeditor/attachment_files";
+  config.filebrowserFlashUploadUrl = "/ckeditor/attachment_files";
+  config.filebrowserImageBrowseLinkUrl = "/ckeditor/pictures";
+  config.filebrowserImageBrowseUrl = "/ckeditor/pictures";
+  config.filebrowserImageUploadUrl = "/ckeditor/pictures";
+  config.filebrowserUploadUrl = "/ckeditor/attachment_files";
 };
