7637ec68f717f224015248f8684b988a55a7f6ab
Paul Carvill <paul@paulcarvill.com>
2014-11-10 20:33:05 +0000

    launch fixesgit status

--- config/environment.rb
+++ config/environment.rb
@@ -1,5 +1,8 @@
 # Load the Rails application.
 require File.expand_path('../application', __FILE__)
 
+# this enables creation of casein cmas accounts
+ActionMailer::Base.smtp_settings[:enable_starttls_auto] = false
+
 # Initialize the Rails application.
 Rails.application.initialize!
